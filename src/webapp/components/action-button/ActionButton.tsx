fceebf7a4114a48240d90df2bdeb20c8662a312b
Alexis Rico <sferadev@gmail.com>
2020-08-29 19:06:20 +0200

    Refactor styling to styled components

--- src/webapp/components/action-button/ActionButton.tsx
+++ src/webapp/components/action-button/ActionButton.tsx
@@ -1,45 +1,41 @@
 import { Fab } from "@material-ui/core";
 import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
 import React from "react";
-import css from "styled-jsx/macro";
+import styled from "styled-components";
 import i18n from "../../../locales";
 
 export interface ActionButtonProps {
     onClick: () => void;
 }
 
-const { className, styles } = css.resolve`
-    .MuiFab-root {
-        position: fixed;
-        margin: 10px;
-        bottom: 20px;
-        right: 40px;
-        display: inline-flex;
-        align-items: center;
-        color: #fff;
-        padding: 15px 20px;
-        background-color: #3c8dbc;
-        border-color: #367fa9;
-        border-radius: 100px;
-        cursor: pointer;
+export const ActionButton: React.FC<ActionButtonProps> = ({ onClick }) => {
+    return (
+        <StyledFab variant="extended" size="large" color="primary" onClick={onClick}>
+            <EmojiObjectsIcon />
+            <p>{i18n.t("Tutorial")}</p>
+        </StyledFab>
+    );
+};
+
+const StyledFab = styled(Fab)`
+    position: fixed;
+    margin: 10px;
+    bottom: 20px;
+    right: 40px;
+    display: inline-flex;
+    align-items: center;
+    color: #fff;
+    padding: 15px 20px;
+    background-color: #3c8dbc;
+    border-color: #367fa9;
+    border-radius: 100px;
+    cursor: pointer;
+
+    :hover {
+        background-color: #296282;
     }
-    .MuiFab-root svg {
+
+    svg {
         margin-right: 6px;
     }
 `;
-
-export const ActionButton: React.FC<ActionButtonProps> = ({ onClick }) => {
-    return (
-        <Fab
-            className={className}
-            variant="extended"
-            size="large"
-            color="primary"
-            onClick={onClick}
-        >
-            <EmojiObjectsIcon />
-            <p>{i18n.t("Tutorial")}</p>
-            {styles}
-        </Fab>
-    );
-};
