c8dfb532d2206e4ffb663b18513815d042f732ec
Alexis Rico <sferadev@gmail.com>
2020-08-29 10:23:46 +0200

    Add basic styling for action button

--- src/webapp/components/action-button/ActionButton.tsx
+++ src/webapp/components/action-button/ActionButton.tsx
@@ -1,4 +1,5 @@
-import { Fab, makeStyles } from "@material-ui/core";
+import { Fab } from "@material-ui/core";
+import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
 import React from "react";
 import i18n from "../../../locales";
 
@@ -7,26 +8,29 @@ export interface ActionButtonProps {
 }
 
 export const ActionButton: React.FC<ActionButtonProps> = ({ onClick }) => {
-    const classes = useStyles();
-
     return (
-        <Fab
-            className={classes.fab}
-            variant="extended"
-            size="large"
-            color="primary"
-            onClick={onClick}
-        >
-            {i18n.t("Tutorial")}
+        <Fab variant="extended" size="large" color="primary" onClick={onClick}>
+            <EmojiObjectsIcon />
+            <p>{i18n.t("Tutorial")}</p>
+            <style>{`
+                .MuiFab-root {
+                    position: fixed;
+                    margin: 10px;
+                    bottom: 20px;
+                    right: 40px;
+                    display: inline-flex;
+                    align-items: center;
+                    color: #fff;
+                    padding: 15px 20px;
+                    background-color: #3c8dbc;
+                    border-color: #367fa9;
+                    border-radius: 100px;
+                    cursor: pointer;
+                }
+                .MuiFab-root svg {
+                    margin-right: 6px;
+                }
+            `}</style>
         </Fab>
     );
 };
-
-const useStyles = makeStyles(theme => ({
-    fab: {
-        position: "fixed",
-        margin: theme.spacing(1),
-        bottom: theme.spacing(4),
-        right: theme.spacing(8),
-    },
-}));
