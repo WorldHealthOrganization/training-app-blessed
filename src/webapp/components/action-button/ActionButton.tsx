f93ec86691ae7468d122406a45b3bc5162ab7d50
Alexis Rico <sferadev@gmail.com>
2020-08-29 18:15:27 +0200

    Use styled-jsx

--- src/webapp/components/action-button/ActionButton.tsx
+++ src/webapp/components/action-button/ActionButton.tsx
@@ -1,36 +1,45 @@
 import { Fab } from "@material-ui/core";
 import EmojiObjectsIcon from "@material-ui/icons/EmojiObjects";
 import React from "react";
+import css from "styled-jsx/macro";
 import i18n from "../../../locales";
 
 export interface ActionButtonProps {
     onClick: () => void;
 }
 
+const { className, styles } = css.resolve`
+    .MuiFab-root {
+        position: fixed;
+        margin: 10px;
+        bottom: 20px;
+        right: 40px;
+        display: inline-flex;
+        align-items: center;
+        color: #fff;
+        padding: 15px 20px;
+        background-color: #3c8dbc;
+        border-color: #367fa9;
+        border-radius: 100px;
+        cursor: pointer;
+    }
+    .MuiFab-root svg {
+        margin-right: 6px;
+    }
+`;
+
 export const ActionButton: React.FC<ActionButtonProps> = ({ onClick }) => {
     return (
-        <Fab variant="extended" size="large" color="primary" onClick={onClick}>
+        <Fab
+            className={className}
+            variant="extended"
+            size="large"
+            color="primary"
+            onClick={onClick}
+        >
             <EmojiObjectsIcon />
             <p>{i18n.t("Tutorial")}</p>
-            <style>{`
-                .MuiFab-root {
-                    position: fixed;
-                    margin: 10px;
-                    bottom: 20px;
-                    right: 40px;
-                    display: inline-flex;
-                    align-items: center;
-                    color: #fff;
-                    padding: 15px 20px;
-                    background-color: #3c8dbc;
-                    border-color: #367fa9;
-                    border-radius: 100px;
-                    cursor: pointer;
-                }
-                .MuiFab-root svg {
-                    margin-right: 6px;
-                }
-            `}</style>
+            {styles}
         </Fab>
     );
 };
