d650a46d6e769c12c3f3e8073e31784082d1c5f3
Alexis Rico <sferadev@gmail.com>
2020-08-30 19:28:43 +0200

    Add basic wizard solution

--- src/webapp/components/draggable-dialog/DraggableDialog.tsx
+++ src/webapp/components/draggable-dialog/DraggableDialog.tsx
@@ -1,32 +1,99 @@
-import { Button, DialogActions, DialogTitle } from "@material-ui/core";
 import { Paper } from "material-ui";
 import React from "react";
 import Draggable from "react-draggable";
+import styled from "styled-components";
+import { Wizard, WizardStep } from "../wizard/Wizard";
 
 export interface DraggableDialogProps {
     open: boolean;
     setOpen: (open: boolean) => void;
 }
 
-export const DraggableDialog: React.FC<DraggableDialogProps> = ({ open, setOpen }) => {
-    const handleClose = () => {
-        setOpen(false);
-    };
-
+export const DraggableDialog: React.FC<DraggableDialogProps> = ({ open }) => {
     if (!open) return null;
+
+    const onStepChangeRequest = async (_currentStep: WizardStep, _newStep: WizardStep) => {
+        return undefined;
+    };
 
     return (
         <Draggable handle="#draggable-dialog-title">
-            <Paper style={{ width: 300 }}>
-                <DialogTitle style={{ cursor: "move" }} id="draggable-dialog-title">
-                    Tutorial
-                </DialogTitle>
-                <DialogActions>
-                    <Button autoFocus onClick={handleClose} color="primary">
-                        Close
-                    </Button>
-                </DialogActions>
+            <Paper style={{ width: 500 }}>
+                <StyledWizard
+                    useSnackFeedback={true}
+                    onStepChangeRequest={onStepChangeRequest}
+                    initialStepKey={"general-info"}
+                    steps={[
+                        {
+                            key: "general-info",
+                            label: "General info",
+                            component: GeneralInfoStep,
+                        },
+                        {
+                            key: "general-info2",
+                            label: "General info",
+                            component: GeneralInfoStep,
+                        },
+                    ]}
+                />
             </Paper>
         </Draggable>
+    );
+};
+
+const StyledWizard = styled(Wizard)`
+    .MuiStepConnector-root {
+        display: none;
+    }
+
+    .MuiStepLabel-labelContainer {
+        display: none;
+    }
+
+    .MuiStepLabel-iconContainer > svg {
+        font-weight: 700;
+        border: 0px;
+        background-color: #fff;
+        padding: 5px;
+        border-radius: 100px;
+        height: 20px;
+        width: 20px;
+        color: #276696;
+        display: inline-block;
+        line-height: 20px;
+        color: #276696;
+        background-color: #43cbcb;
+    }
+
+    circle {
+        display: none;
+    }
+
+    .MuiStepLabel-iconContainer::after {
+        content: "";
+        position: absolute;
+        top: 50%;
+        height: 3px;
+        width: calc(100% - 21px);
+        background-color: #43cbcb;
+        margin-left: 10px;
+    }
+
+    .Wizard-Stepper > button {
+        font-size: 18px;
+        color: #fff;
+        margin: 0px 20px;
+        padding: 15px 36px;
+        border-radius: 100px;
+        border: 0px;
+        background-color: #43cbcb;
+    }
+`;
+
+const GeneralInfoStep = () => {
+    return (
+        <React.Fragment>
+            <p>This is a step</p>
+        </React.Fragment>
     );
 };
