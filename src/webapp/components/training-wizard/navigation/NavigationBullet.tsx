b26f88401439170552c5654c8f7da51f89465152
Alexis Rico <sferadev@gmail.com>
2020-09-03 09:31:28 +0200

    Make sub bullets round again

--- src/webapp/components/training-wizard/navigation/NavigationBullet.tsx
+++ src/webapp/components/training-wizard/navigation/NavigationBullet.tsx
@@ -7,7 +7,6 @@ const SubBullet = (props: { className?: string; completed?: boolean }) => (
 
 export const NavigationBullet = styled(SubBullet)`
     text-align: center;
-    width: 20%;
     position: relative;
 
     font-weight: 700;
@@ -16,7 +15,6 @@ export const NavigationBullet = styled(SubBullet)`
     width: 10px;
     display: inline-block;
     line-height: 34px;
-    margin: 18px 10px 0 10px;
 
     background-color: ${props => (props.completed ? "#43cbcb" : "rgba(255, 255, 255, 0.3)")};
 `;
