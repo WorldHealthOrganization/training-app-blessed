a52f4d4135799cdabfe23c468ad00dcd4e5954b2
Alexis Rico <sferadev@gmail.com>
2020-09-08 12:47:57 +0200

    Add namespace to debug call

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -2,5 +2,5 @@ const isDevelopment = process.env.NODE_ENV === "development";
 const isTest = process.env.JEST_WORKER_ID !== undefined;
 
 export const log = (...message: unknown[]) => {
-    if (isDevelopment && !isTest) console.log(...message);
+    if (isDevelopment && !isTest) console.log("[Training]", ...message);
 };
