2eb15ce8cec4df573a85b7c5720005443cdccbba
Alexis Rico <sferadev@gmail.com>
2020-09-09 21:02:44 +0200

    Add final page

--- src/webapp/pages/final/FinalPage.tsx
+++ src/webapp/pages/final/FinalPage.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import DataEntryIcon from "../../assets/data-entry/Icon.png";
 import Decoration from "../../assets/Decoration.png";
 import { MainButton } from "../../components/main-button/MainButton";
 import {
@@ -14,34 +13,16 @@ import { Stepper } from "../../components/training-wizard/stepper/Stepper";
 import { steps } from "../../components/training-wizard/TrainingWizard";
 
 export const FinalPage = () => {
-    return true ? (
+    return (
         <StyledModal>
             <ModalContent>
-                <span className="modalbigTitle spaced">Well done!</span>
-                <span className="modalSmallTitle">You’ve completed the data entry tutorial!</span>
+                <ModalTitle big={true}>Well done!</ModalTitle>
+                <ModalParagraph>You’ve completed the data entry tutorial!</ModalParagraph>
+                <Stepper steps={steps} lastClickableStepIndex={-1} markAllCompleted={true} />
+                <ModalFooter>
+                    <MainButton>Next</MainButton>
+                </ModalFooter>
             </ModalContent>
-            <Stepper steps={steps} lastClickableStepIndex={-1} markAllCompleted={true} />
-            <ModalFooter>
-                <MainButton>Next</MainButton>
-            </ModalFooter>
-        </StyledModal>
-    ) : (
-        <StyledModal>
-            <ModalContent>
-                <ModalTitle big={true}>Welcome to the tutorial for Data Entry</ModalTitle>
-                <Image>
-                    <img src={DataEntryIcon} alt="Welcome Illustration" />
-                </Image>
-                <ModalParagraph>
-                    The data entry application is used to enter data that need to be entered for one
-                    location on a regular basis such as weekly, monthy etc. Data is registered for a
-                    location, time period and a specific dataset.{" "}
-                </ModalParagraph>
-            </ModalContent>
-            <ModalFooter>
-                <MainButton color="secondary">Exit Tutorial</MainButton>
-                <MainButton color="primary">Start Tutorial</MainButton>
-            </ModalFooter>
         </StyledModal>
     );
 };
@@ -56,9 +37,22 @@ const StyledModal = styled(Modal)`
     background-position: center; /* Center the image */
     background-repeat: no-repeat; /* Do not repeat the image */
     height: 600px;
-`;
 
-const Image = styled.span`
-    display: block;
-    margin: 18px 0px;
+    ${ModalContent} {
+        max-width: none;
+        width: 700px;
+        position: relative;
+        top: 25%;
+    }
+
+    ${ModalParagraph} {
+        font-size: 25px;
+        line-height: 32px;
+        font-weight: 300;
+        margin: 25px 0px 15px 0px;
+    }
+
+    ${ModalFooter} {
+        margin-top: 20px;
+    }
 `;
