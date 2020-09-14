8da4f7a060f50ea942d3edad4adb069c223a5874
Alexis Rico <sferadev@gmail.com>
2020-09-14 09:36:15 +0200

    Add markdown parsing

--- src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
+++ src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import styled from "styled-components";
 import { ModalContent } from "../../modal/ModalContent";
 
 export const GeneralInfoStep = () => {
+    const input =
+        "# This is a header\n\nAnd this is a paragraph\n\nAnd a **bold** text\n\n![alt text](http://qnimate.com/wp-content/uploads/2014/03/images2.jpg)";
+
     return (
         <ModalContent>
-            <p style={{ margin: 0, color: "#fff" }}>
-                {"Placeholder for the contents ".repeat(100)}
-            </p>
+            <Markdown source={input} escapeHtml={true} skipHtml={true} />
         </ModalContent>
     );
 };
+
+const Markdown = styled(ReactMarkdown)`
+    color: white;
+
+    img {
+        width: 90%;
+    }
+`;
