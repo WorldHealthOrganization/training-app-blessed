8fa38f9fbb0441de99e0cad192773081e9a48005
Alexis Rico <sferadev@gmail.com>
2020-09-14 11:46:16 +0200

    Adjust styles for markdown parser

--- src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
+++ src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
@@ -5,19 +5,20 @@ import { ModalContent } from "../../modal/ModalContent";
 
 export const GeneralInfoStep = () => {
     const input =
-        "# This is a header\n\nAnd this is a paragraph\n\nAnd a **bold** text\n\n![alt text](http://qnimate.com/wp-content/uploads/2014/03/images2.jpg)";
+        "# This is a header\n\nAnd this is a paragraph\n\nAnd a **bold** text\n\n<p align='right'>This is left aligned</p>\n\n![alt text](http://qnimate.com/wp-content/uploads/2014/03/images2.jpg)";
 
     return (
         <ModalContent>
-            <Markdown source={input} escapeHtml={true} skipHtml={true} />
+            <Markdown source={input} escapeHtml={false} />
         </ModalContent>
     );
 };
 
 const Markdown = styled(ReactMarkdown)`
     color: white;
+    padding: 0 20px 0 20px;
 
     img {
-        width: 90%;
+        width: 100%;
     }
 `;
