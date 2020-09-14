7a39b72ae060146321a47895ed790c81cafc2328
Alexis Rico <sferadev@gmail.com>
2020-09-14 11:53:49 +0200

    Fix typo

--- src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
+++ src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
@@ -5,7 +5,7 @@ import { ModalContent } from "../../modal/ModalContent";
 
 export const GeneralInfoStep = () => {
     const input =
-        "# This is a header\n\nAnd this is a paragraph\n\nAnd a **bold** text\n\n<p align='right'>This is left aligned</p>\n\n![alt text](http://qnimate.com/wp-content/uploads/2014/03/images2.jpg)";
+        "# This is a header\n\nAnd this is a paragraph\n\nAnd a **bold** text\n\n<p align='right'>This is right aligned</p>\n\n![alt text](http://qnimate.com/wp-content/uploads/2014/03/images2.jpg)";
 
     return (
         <ModalContent>
