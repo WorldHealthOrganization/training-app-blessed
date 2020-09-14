022421f5de51c954cbd2a17caec9b88a200b74d7
Alexis Rico <sferadev@gmail.com>
2020-09-14 12:03:05 +0200

    Add more styles to step

--- src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
+++ src/webapp/components/training-wizard/steps/GeneralInfoStep.tsx
@@ -18,6 +18,19 @@ const Markdown = styled(ReactMarkdown)`
     color: white;
     padding: 0 20px 0 20px;
 
+    h1 {
+        font-size: 36px;
+        line-height: 47px;
+        font-weight: 300;
+        margin: 0px 0px 30px 0px;
+    }
+
+    p {
+        font-size: 18px;
+        font-weight: 300;
+        line-height: 28px;
+    }
+
     img {
         width: 100%;
     }
