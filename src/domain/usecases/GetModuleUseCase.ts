7c889062c440653d7a5407e3416177c20a23300d
Alexis Rico <sferadev@gmail.com>
2020-09-20 11:03:23 +0200

    Update contents with images

--- src/domain/usecases/GetModuleUseCase.ts
+++ src/domain/usecases/GetModuleUseCase.ts
@@ -45,7 +45,7 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "Another way to find your location (organisation unit) is to search the hierarchy tree menu.\n\nExpand and close the branches by clicking on the +/- symbols. Click on your organisation unit name to highlight it orange and this will enter the location in the Organisation Unit field of your data entry form. ",
+                                "![location2](https://user-images.githubusercontent.com/2181866/93707577-35553500-fb30-11ea-99be-b835bc5da7bf.png)\n\nAnother way to find your location (organisation unit) is to search the hierarchy tree menu.\n\nExpand and close the branches by clicking on the +/- symbols. Click on your organisation unit name to highlight it orange and this will enter the location in the Organisation Unit field of your data entry form. ",
                         },
                     ],
                 },
@@ -56,7 +56,7 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "Select a data set from the dropdown list of data sets available to your selected location. Note that not all data sets are available for all organisation units. The data set you select will create a form with similar fields to your paper based form. ",
+                                "![dataset](https://user-images.githubusercontent.com/2181866/93707575-34bc9e80-fb30-11ea-9994-0e0175a191b5.png)\n\nSelect a data set from the dropdown list of data sets available to your selected location. Note that not all data sets are available for all organisation units. The data set you select will create a form with similar fields to your paper based form. ",
                         },
                     ],
                 },
@@ -67,12 +67,12 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "Select a period for which to register data. The available periods are determined by the reporting frequency required for the selected form.",
+                                "![time1](https://user-images.githubusercontent.com/2181866/93707580-35edcb80-fb30-11ea-832b-704d90d0f1a4.png)\n\nSelect a period for which to register data. The available periods are determined by the reporting frequency required for the selected form.",
                         },
                         {
                             type: "markdown",
                             text:
-                                "You can also jump a year back or forward by using the arrows next to the period.",
+                                "![time2](https://user-images.githubusercontent.com/2181866/93707581-36866200-fb30-11ea-9f4d-8909e5c690fb.png)\n\nYou can also jump a year back or forward by using the arrows next to the period.",
                         },
                     ],
                 },
@@ -83,7 +83,7 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "By now you should see the data entry form. If not, there may be additional data dimensions that you need to select from drop-down menus, depending on how your form is structured. This may include the type of reporting institution, project or other features.",
+                                "![dataset](https://user-images.githubusercontent.com/2181866/93707575-34bc9e80-fb30-11ea-9994-0e0175a191b5.png)\n\nBy now you should see the data entry form. If not, there may be additional data dimensions that you need to select from drop-down menus, depending on how your form is structured. This may include the type of reporting institution, project or other features.",
                         },
                     ],
                 },
@@ -94,7 +94,7 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "Start entering data by clicking inside the first cell and either typing in the values or selecting them from the dropdown menus.\n\nThe values are saved immediately and do not require any save/finished button click. A green field indicates that the value has been saved in the system. Grey fields are those that are completed automatically by the DHIS2 system.",
+                                "![data](https://user-images.githubusercontent.com/2181866/93707574-34240800-fb30-11ea-973c-d0f6622af076.png)\n\nStart entering data by clicking inside the first cell and either typing in the values or selecting them from the dropdown menus.\n\nThe values are saved immediately and do not require any save/finished button click. A green field indicates that the value has been saved in the system. Grey fields are those that are completed automatically by the DHIS2 system.",
                         },
                     ],
                 },
@@ -105,7 +105,7 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "If you type in an invalid value or a value that is outside of a valid range, you will get a pop-up that explains the problem and the field will be coloured yellow (not saved) until you have corrected the value.",
+                                "![correct](https://user-images.githubusercontent.com/2181866/93707583-36866200-fb30-11ea-957e-7bd91f5e04da.png)\n\nIf you type in an invalid value or a value that is outside of a valid range, you will get a pop-up that explains the problem and the field will be coloured yellow (not saved) until you have corrected the value.",
                         },
                     ],
                 },
@@ -116,7 +116,7 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "Click “Run validation” to check that all the data that you have entered meet the rules built-in to the form for ensuring accuracy of the data.",
+                                "![validation](https://user-images.githubusercontent.com/2181866/93707582-36866200-fb30-11ea-8736-2ae535147343.png)\n\nClick “Run validation” to check that all the data that you have entered meet the rules built-in to the form for ensuring accuracy of the data.",
                         },
                     ],
                 },
@@ -127,7 +127,7 @@ export class GetModuleUseCase implements UseCase {
                         {
                             type: "markdown",
                             text:
-                                "Click “complete” to submit the data that you have entered to DHIS2.",
+                                "![submit](https://user-images.githubusercontent.com/2181866/93707579-35edcb80-fb30-11ea-8451-1d3d2cab6bf6.png)\n\nClick “complete” to submit the data that you have entered to DHIS2.",
                         },
                     ],
                 },
