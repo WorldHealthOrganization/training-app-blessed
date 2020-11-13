84571107b69e20fa0a7c116eb2ace09cdcc95946
Alexis Rico <sferadev@gmail.com>
2020-11-13 12:00:48 +0100

    Build abstraction layer between poeditor and translations

--- src/data/clients/translation/TranslationClient.ts
+++ src/data/clients/translation/TranslationClient.ts
@@ -1,3 +1,11 @@
+import { Either } from "../../../domain/entities/Either";
+import { TranslationProject } from "../../entities/Translation";
+
 export type TranslationProvider = "poeditor";
 
 export type TranslationError = "UNKNOWN";
+
+export interface TranslationClient {
+    getProject(id: string): Promise<Either<TranslationError, TranslationProject>>;
+    createProject(): Promise<TranslationProject>;
+}
--- src/data/entities/Translation.ts
+++ src/data/entities/Translation.ts
@@ -5,3 +5,14 @@ export interface Translation {
     provider: TranslationProvider;
     projectId: string;
 }
+
+export interface TranslationProject {
+    id: string;
+    name: string;
+    languages: TranslationLanguage[];
+}
+
+export interface TranslationLanguage {
+    id: string;
+    name: string;
+}
