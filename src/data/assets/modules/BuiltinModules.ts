ebf163cdee3166b4e6f07cded0ed051e89be16a4
Alexis Rico <sferadev@gmail.com>
2020-11-18 08:50:59 +0100

    Add event visualizer module

--- src/data/assets/modules/BuiltinModules.ts
+++ src/data/assets/modules/BuiltinModules.ts
@@ -1,9 +1,11 @@
 import DashboardModule from "./01-dashboards-module.json";
 import DataEntryModule from "./02-data-entry-module.json";
 import EventCaptureModule from "./03-event-capture-module.json";
+import EventVisualizerModule from "./04-event-visualizer-module.json";
 
 export const BuiltinModules = {
     dashboards: DashboardModule,
     "data-entry": DataEntryModule,
     "event-capture": EventCaptureModule,
+    "event-visualizer": EventVisualizerModule,
 };
