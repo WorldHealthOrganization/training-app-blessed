883fce9d9a27c5f89b21bf1bbb8b436c459be6e6
Alexis Rico <sferadev@gmail.com>
2020-11-26 11:18:32 +0100

    Add pivot tables and maps modules

--- src/data/assets/modules/BuiltinModules.ts
+++ src/data/assets/modules/BuiltinModules.ts
@@ -2,10 +2,16 @@ import DashboardModule from "./01-dashboards-module.json";
 import DataEntryModule from "./02-data-entry-module.json";
 import EventCaptureModule from "./03-event-capture-module.json";
 import EventVisualizerModule from "./04-event-visualizer-module.json";
+import DataVisualizerModule from "./05-data-visualizer-module.json";
+import PivotTablesModule from "./06-pivot-tables-module.json";
+import MapsModule from "./07-maps-module.json";
 
 export const BuiltinModules = {
     dashboards: DashboardModule,
     "data-entry": DataEntryModule,
     "event-capture": EventCaptureModule,
     "event-visualizer": EventVisualizerModule,
+    "data-visualizer": DataVisualizerModule,
+    "pivot-tables": PivotTablesModule,
+    maps: MapsModule,
 };
