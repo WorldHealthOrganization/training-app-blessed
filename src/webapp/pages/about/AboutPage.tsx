f7ccdf3d9a8e29984dfe6ccede4cd506d1eec7f1
Alexis Rico <sferadev@gmail.com>
2021-06-23 13:35:56 +0200

    Update about text

--- src/webapp/pages/about/AboutPage.tsx
+++ src/webapp/pages/about/AboutPage.tsx
@@ -18,6 +18,10 @@ export const AboutPage = () => {
             "This application has been entirely funded by the WHO Global Malaria Programme to support countries using DHIS2 in strengthening the collection and use of health data. The application has been designed by [Lushomo](https://lushomo.net) and developed by [EyeSeeTea SL](http://eyeseetea.com). The source code and release notes can be found at the [WHO GitHub repository](https://github.com/WorldHealthOrganization/training-app-blessed). If you wish to contribute to the development of Training App with new features, please contact [EyeSeeTea](mailto:hello@eyeseetea.com). To continue developing the tool in a coordinated manner please always contact also [WHO](mailto:integrated-data@who.int)",
             { nsSeparator: false }
         ),
+        i18n.t(
+            "*Disclaimer: The WHO has developed this application to support countries build capacity for health data collection and use. WHO provides a series of tutorials to support countries to use the WHO DHIS2 standard packages which can be found in the [WHO Tutorial GitHub repository](https://github.com/WorldHealthOrganization/DHIS2-tutorials) and can be installed in the application. WHO provides no assurance as to the validity, accuracy or completeness of any other tutorials built by the application's user community.*",
+            { nsSeparator: false }
+        ),
     ].join("\n\n");
 
     const goHome = useCallback(() => {
